a326c3a27a8514d6d5df96c5cbe654322bcec485
unknown <cxb4846@NM2550-25.main.ad.rit.edu>
2013-12-08 18:11:56 -0500

    Commented Combat.cs and Form1.cs

--- Final Combat/Combats.cs
+++ Final Combat/Combats.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Final_Combat
 {
-    enum EInput
+    enum EInput //enum for player input
     {
         Attack = 1,
         Defend = 2,
@@ -17,93 +17,99 @@ namespace Final_Combat
     class Combats
     {
 
-        static Random roll; 
+        static Random roll; //random for battle-end regen
 
         public Combats()
         {
             roll = new Random();
         }
 
-        public bool IsAlive(int health)
+        /// <summary>
+        /// Checks if the player is alive.
+        /// </summary>
+        /// <param name="health"></param>
+        /// <returns></returns>
+        public bool IsAlive(int health) 
         {
             return (health > 0);
         }
 
-        private bool combat = false;
+        private bool combat = false; //a bool to see if the player is in combat
+        /// <summary>
+        /// Gets and sets the boolean to check if the player is in combat. 
+        /// </summary>
         public bool Combat { get { return combat; } set { combat = value; } }
 
-        string output = "";
+        string output = ""; //output for the text box
 
-        //public void Fight()
-        //{
-        //    while (combat == true)
-        //    {
-        //
-        //        Battle(notDude.WCombatAI(notDude.Health));
-        //    }
-        //}
-
-        public string Battle(EInput userInput, EInput enemyInput, Base player, Base enemy)
+        /// <summary>
+        /// The main battle method that executes the player and enemy actions. 
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <param name="enemyInput"></param>
+        /// <param name="player"></param>
+        /// <param name="enemy"></param>
+        /// <returns></returns>
+        public string Battle(EInput userInput, EInput enemyInput, Base player, Base enemy) 
         {
             output = "";
-            bool pFirst = (player.Dexterity >= enemy.Dexterity);
+            bool pFirst = (player.Dexterity >= enemy.Dexterity); //who goes first
             if (combat == true)
             {
-                        if (pFirst == true)
-                        {
+                if (pFirst == true) //if player first
+                {
 
-                            if (player.Health > 0)
-                            {
-                                int playerOut = player.Combat(userInput, player, enemy);
-                                if (userInput == EInput.Potion)
-                                    output = output + "\nYou heal for " + playerOut + ".";
-                                else if (userInput == EInput.Defend)
-                                    output = output + "\nYou defend for " + playerOut + ".";
-                                else
-                                    output = output + "\nYou deal " + playerOut + " damage.";
-                            }
-                            else
-                                combat = false;
-                            if (enemy.Health > 0)
-                            {
-                                int enemyOut = enemy.Combat(enemyInput, enemy, player);
-                                if (enemyInput == EInput.Potion)
-                                    output = output + "\nThey heal for " + enemyOut + ".";
-                                else
-                                    output = output + "\nThey deal " + enemyOut + " damage.";
-                            }
-                            else
-                                combat = false;
-                        }
-
+                    if (player.Health > 0) //checks if the player is still alive
+                    {
+                        int playerOut = player.Combat(userInput, player, enemy); //performs the player action and prints an appropriate output
+                        if (userInput == EInput.Potion)
+                            output = output + "\nYou heal for " + playerOut + ".";
+                        else if (userInput == EInput.Defend)
+                            output = output + "\nYou defend for " + playerOut + ".";
                         else
-                        {
-                            int enemyOut = enemy.Combat(userInput, enemy, player);
-                            if (enemyInput == EInput.Potion)
-                                output = output + "\nThey heal for " + enemyOut + ".";
-                            else if (enemyInput == EInput.Defend)
-                                output = output + "\nThey defend for " + enemyOut + ".";
-                            else
-                                output = output + "\nThey deal " + enemyOut + " damage.";
-                            if (player.Health > 0)
-                            {
-                                int playerOut = player.Combat(enemyInput, enemy, player);
-                                if (userInput == EInput.Potion)
-                                    output = output + "\nYou heal for " + playerOut + ".";
-                                else
-                                    output = output + "\nYou deal " + playerOut + " damage.";
-                            }
-                            else
-                                combat = false;
-                        }
+                            output = output + "\nYou deal " + playerOut + " damage.";
+                    }
+                    else
+                        combat = false;
+                    if (enemy.Health > 0) //enemy action
+                    {
+                        int enemyOut = enemy.Combat(enemyInput, enemy, player);
+                        if (enemyInput == EInput.Potion)
+                            output = output + "\nThey heal for " + enemyOut + ".";
+                        else
+                            output = output + "\nThey deal " + enemyOut + " damage.";
+                    }
+                    else
+                        combat = false;
+                }
 
+                else //if enemy first
+                {
+                    int enemyOut = enemy.Combat(userInput, enemy, player);
+                    if (enemyInput == EInput.Potion)
+                        output = output + "\nThey heal for " + enemyOut + ".";
+                    else if (enemyInput == EInput.Defend)
+                        output = output + "\nThey defend for " + enemyOut + ".";
+                    else
+                        output = output + "\nThey deal " + enemyOut + " damage.";
+                    if (player.Health > 0) //player's turn if alive
+                    {
+                        int playerOut = player.Combat(enemyInput, enemy, player);
+                        if (userInput == EInput.Potion)
+                            output = output + "\nYou heal for " + playerOut + ".";
+                        else
+                            output = output + "\nYou deal " + playerOut + " damage.";
+                    }
+                    else
+                        combat = false;
+                }
             }
-            if ((player.Health > 0) == true && (enemy.Health > 0) == false)
+            if ((player.Health > 0) == true && (enemy.Health > 0) == false) //victory
             {
                 player.Health += roll.Next(1, 11);
                 output = output + "\nYou win!";
             }
-            else if ((player.Health > 0) == false)
+            else if ((player.Health > 0) == false) //defeat
                 output = output + "\n You have died!";
             return output;
         }
--- Final Combat/Form1.cs
+++ Final Combat/Form1.cs
@@ -14,7 +14,7 @@ namespace Final_Combat
     
     public partial class Form1 : Form
     {
-        Combats FIGHT;
+        Combats FIGHT; //instantiating the combat method, player, an enemy, and the dungeon
         Warrior player;
         EWarrior enemy1;
         Dungeon dungeon; 
@@ -30,24 +30,32 @@ namespace Final_Combat
             enemy1 = new EWarrior(0, 0);
             FIGHT.Combat = true;
             playerStats.Text = player.ToString();
-            CombatVisCheck();
+            CombatVisCheck(); //to set up the GUI
+            combatText.Text += "You have entered combat! What do you do?";
         }
 
-        private void CombatVisCheck()
+        /// <summary>
+        /// Check to see if combat continues.
+        /// </summary>
+        private void CombatVisCheck() 
         {
-            if (player.Health > 0 && enemy1.Health > 0)
+            if (player.Health > 0 && enemy1.Health > 0) //if both are alive
                 CombatVisibility(true);
             else
             {
                 CombatVisibility(false);
-                if (player.Health > 0)
+                if (player.Health > 0) //player wins
                     MessageBox.Show("You have won the combat!", "Victory", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                else
+                else //player loses
                     MessageBox.Show("You have died!", "Defeat", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private void CombatVisibility(bool combat)
+        /// <summary>
+        /// Sets visibility to the combat bool.
+        /// </summary>
+        /// <param name="combat"></param>
+        private void CombatVisibility(bool combat) 
         {
             attackButton.Visible = combat;
             defendButton.Visible = combat;
@@ -62,53 +70,76 @@ namespace Final_Combat
             potionButton.Enabled = combat;
             combatText.Enabled = combat;
             enemyStats.Enabled = combat;
-            enemyStats.Text = enemy1.ToString();
-            combatText.Text += "You have entered combat! What do you do?";
+            enemyStats.Text = enemy1.ToString(); //loads enemy stats
+
         }
 
-        private void attackButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Calls the attack action for the player.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void attackButton_Click(object sender, EventArgs e) 
         {
             EInput userInput = EInput.Attack;
             string output = FIGHT.Battle(userInput, enemy1.WCombatAI(enemy1.Health), player, enemy1);
             combatText.Text = combatText.Text + output;
-            playerStats.Text = player.ToString();
+            playerStats.Text = player.ToString(); //update stats
             enemyStats.Text = enemy1.ToString();
             CombatVisCheck();
         }
 
-        private void defendButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Calls the defend action for the player.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void defendButton_Click(object sender, EventArgs e) 
         {
             EInput userInput = EInput.Defend;
             string output = FIGHT.Battle(userInput, enemy1.WCombatAI(enemy1.Health), player, enemy1);
             combatText.Text = combatText.Text + output;
-            playerStats.Text = player.ToString();
+            playerStats.Text = player.ToString(); //update stats
             enemyStats.Text = enemy1.ToString();
+            CombatVisCheck();
         }
          
-        private void magicButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Calls the magic action for the player.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void magicButton_Click(object sender, EventArgs e) 
         {
             EInput userInput = EInput.Magic;
             string output = FIGHT.Battle(userInput, enemy1.WCombatAI(enemy1.Health), player, enemy1);
             combatText.Text = combatText.Text + output;
-            playerStats.Text = player.ToString();
+            playerStats.Text = player.ToString(); //updates
             enemyStats.Text = enemy1.ToString();
+            CombatVisCheck();
         }
 
-        private void potionButton_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Calls the potion action for the player.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void potionButton_Click(object sender, EventArgs e) 
         {
             EInput userInput = EInput.Potion;
             string output = FIGHT.Battle(userInput, enemy1.WCombatAI(enemy1.Health), player, enemy1);
             combatText.Text = combatText.Text + output;
-            playerStats.Text = player.ToString();
+            playerStats.Text = player.ToString(); //updates
             enemyStats.Text = enemy1.ToString();
+            CombatVisCheck();
         }
 
-        private void combatText_TextChanged(object sender, EventArgs e)
+        private void combatText_TextChanged(object sender, EventArgs e) //empty methods
         {
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void pictureBox1_Click(object sender, EventArgs e) //empty methods
         {
 
         }
